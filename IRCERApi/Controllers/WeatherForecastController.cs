06ee3c96bc5522d1e6d0fdae3e492cc92a20c070
Matthew Pauloksy <matthew.j.paulosky@boeing.com>
2020-04-19 13:32:32 -0700

    Edit the WeatherForecastController and gitignore.

--- IRCERApi/Controllers/WeatherForecastController.cs
+++ IRCERApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IRCERUI.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IRCERApi.Controllers
 {
@@ -11,7 +11,7 @@ namespace IRCERApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] _summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -24,14 +24,16 @@ namespace IRCERApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecastModel> Get()
         {
+            _logger.LogInformation("You have made a request for a Forecast");
+
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastModel
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = _summaries[rng.Next(_summaries.Length)]
             })
             .ToArray();
         }
