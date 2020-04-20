df448cc5c9ab5800748f52eabb771e7b2ec2ba5c
Matthew Pauloksy <matthew.j.paulosky@boeing.com>
2020-04-20 11:52:07 -0700

    Created HomeController Unit Tests
    Created the unit tests for the Index and Privacy Actions.

--- IRCERApi/Controllers/HomeController.cs
+++ IRCERApi/Controllers/HomeController.cs
@@ -1,27 +1,12 @@
 ﻿using IRCERApi.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace IRCERApi.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly UserManager<IdentityUser> _userManager;
-
-        public HomeController(ILogger<HomeController> logger,
-            RoleManager<IdentityRole> roleManager,
-            UserManager<IdentityUser> userManager)
-        {
-            _logger = logger;
-            _roleManager = roleManager;
-            _userManager = userManager;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -35,7 +20,8 @@ namespace IRCERApi.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var err = new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+            return View(err);
         }
     }
 }
---
+++ IRCERApi_UnitTests/HomeControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using Autofac.Extras.Moq;
+using IRCERApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace IRCERApi_UnitTests
+{
+    public class HomeControllerTests
+    {
+        [Fact(DisplayName = "")]
+        public void Index_ReturnsView()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                //Arrange
+
+                var sut = mock.Create<HomeController>();
+
+                //Act
+
+                var result = sut.Index();
+
+                //Assert
+
+                Assert.IsType<ViewResult>(result);
+
+                Assert.True(result != null);
+            }
+        }
+
+        [Fact]
+        public void Privacy_ReturnsView()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                //Arrange
+
+                var sut = mock.Create<HomeController>();
+
+                //Act
+
+                var result = sut.Privacy();
+
+                //Assert
+
+                Assert.IsType<ViewResult>(result);
+
+                Assert.True(result != null);
+            }
+        }
+
+        // ToDo figure out how to test this method
+        //[Fact]
+        //public void Error_ReturnsView()
+        //{
+        //    using (var mock = AutoMock.GetLoose())
+        //    {
+        //        //Arrange
+
+        //        var sut = mock.Create<HomeController>();
+
+        //        //Act
+
+        //        var result = sut.Error();
+
+        //        //Assert
+
+        //        Assert.IsType<ViewResult>(result);
+
+        //        Assert.True(result != null);
+        //    }
+        //}
+    }
+}
