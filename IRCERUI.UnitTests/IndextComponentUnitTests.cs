77d0280ce949df6cbfd7688eac21a7a19869b2b1
Matthew Pauloksy <matthew.j.paulosky@boeing.com>
2020-04-28 14:45:32 -0700

    Added Unit Tests to IRCERUI
    Added Tests for the Counter, Error, and Index components.

---
+++ IRCERUI.UnitTests/IndextComponentUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Bunit;
+using Xunit;
+
+namespace IRCERUI.UnitTests
+{
+    public class IndextComponentUnitTests : ComponentTestFixture
+    {
+        [Fact]
+        public void Component1RendersCorrectly()
+        {
+            var cut = RenderComponent<Pages.Index>();
+
+            var expectedHtml = @"<h1>Hello, world!</h1>
+                                Welcome to your new app.
+                                <div class='alert alert-secondary mt-4' role='alert'>
+                                    <span class='oi oi-pencil mr-2' aria-hidden='true'></span>
+                                  <strong>How is Blazor working for you?</strong>
+                                  <span class='text-nowrap'>
+                                    Please take our
+                                    <a target='_blank' class='font-weight-bold' href='https://go.microsoft.com/fwlink/?linkid=2112271'>brief survey</a>
+                                  </span>
+                                  and tell us what you think.
+                                </div>";
+
+            cut.MarkupMatches(expectedHtml);
+        }
+    }
+}
